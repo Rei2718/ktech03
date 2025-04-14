9ab967010378266455ec3e8c7ccc31889bec9be1
Ishikawa Ichiro <129850397+Rei2718@users.noreply.github.com>
2025-04-14 09:39:46 +0900

    layout.tsx の更新

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter, Roboto_Mono } from "next/font/google";
 import PrelineScriptWrapper from "./components/PrelineScriptWrapper";
 import SplashScreenWrapper from "./components/SplashScreenWrapper";
 
-// Google Fonts (例: Inter と Roboto Mono)
+// Google Fonts
 const inter = Inter({
   variable: "--font-inter",
   subsets: ["latin"],
@@ -19,9 +19,10 @@ const robotoMono = Roboto_Mono({
 export const metadata: Metadata = {
   title: {
     default: 'K-Tech | 立命館慶祥高等学校IT同好会',
-    template: '%s | K-Tech'
+    template: '%s | K-Tech',
   },
-  description: 'K-Tech is a student-driven creative tech club exploring 3DCG, web development, AI, and media art. Join us to turn your passion into real-world innovation.',
+  description:
+    'K-Tech is a student-driven creative tech club exploring 3DCG, web development, AI, and media art. Join us to turn your passion into real-world innovation.',
   keywords: [
     'K-Tech',
     'Ritsumei',
@@ -35,24 +36,25 @@ export const metadata: Metadata = {
     'Data Science',
     'App Development',
     'Innovation',
-    '立命館慶祥高等学校'
+    '立命館慶祥高等学校',
   ],
   metadataBase: new URL('https://keisho.tech/'),
   openGraph: {
     title: 'K-Tech | RitsumeikanKeisho K-Tech',
-    description: 'Explore cutting-edge creativity with K-Tech — where students turn ideas into impact through technology.',
+    description:
+      'Explore cutting-edge creativity with K-Tech — where students turn ideas into impact through technology.',
     url: 'https://keisho.tech/',
     siteName: 'K-Tech',
     images: [
       {
         url: 'logo.png',
-        width:  630,
+        width: 630,
         height: 630,
-        alt: 'K-Tech Official Visual'
-      }
+        alt: 'K-Tech Official Visual',
+      },
     ],
     locale: 'ja_JP',
-    type: 'website'
+    type: 'website',
   },
   robots: {
     index: true,
@@ -62,16 +64,16 @@ export const metadata: Metadata = {
       follow: true,
       maxSnippet: -1,
       maxImagePreview: 'large',
-      maxVideoPreview: -1
-    }
+      maxVideoPreview: -1,
+    },
   },
   alternates: {
     canonical: 'https://keisho.tech/',
     languages: {
-      'ja-JP': 'https://keisho.tech/'
-    }
-  }
-}
+      'ja-JP': 'https://keisho.tech/',
+    },
+  },
+};
 
 export default function RootLayout({
   children,
@@ -79,21 +81,28 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="ja">
       <head>
-        <link rel="icon" type="image/png" href="/favicon/favicon-96x96.png" sizes="96x96" />
+        <link
+          rel="icon"
+          type="image/png"
+          href="/favicon/favicon-96x96.png"
+          sizes="96x96"
+        />
         <link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg" />
         <link rel="shortcut icon" href="/favicon/favicon.ico" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        />
         <meta name="apple-mobile-web-app-title" content="K-Tech" />
         <link rel="manifest" href="/favicon/site.webmanifest" />
       </head>
-      <body className={`${inter.variable} ${robotoMono.variable} antialiased`}>
-        {/* コンテンツの上に SplashScreenWrapper でラップ */}
-        <SplashScreenWrapper>
-          {children}
-        </SplashScreenWrapper>
-        {/* スクリプトも body 内に配置 */}
+      <body
+        className={`${inter.variable} ${robotoMono.variable} antialiased`}
+      >
+        <SplashScreenWrapper>{children}</SplashScreenWrapper>
         <PrelineScriptWrapper />
       </body>
     </html>
