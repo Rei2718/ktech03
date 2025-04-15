b2bd06a1bae8a8768185188d51ce9ec3bf40893d
Ishikawa Ichiro <129850397+Rei2718@users.noreply.github.com>
2025-04-15 18:22:06 +0900

    Mac3.tsx の更新

--- app/components/slide/Mac3.tsx
+++ app/components/slide/Mac3.tsx
@@ -6,7 +6,7 @@ export default function Mac3() {
       <div className="absolute top-0 left-0 z-10 flex flex-col justify-between h-full px-6 sm:px-10 md:px-16 lg:px-24 py-10 sm:py-12 md:py-20">
         <div className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-semibold text-left">
           <div className="pb-6">DATA SCIENCE</div>
-          <div className="text-base sm:text-lg md:text-xl lg:text-xl xl:text-2xl max-w-3xl mx-auto text-[#A2A2A2]">Python・データサイエンス入門</div>
+          <div className="text-base sm:text-lg md:text-xl lg:text-xl xl:text-2xl max-w-3xl mx-auto">Python・データサイエンス入門</div>
         </div>
       </div>
       <div className="absolute inset-0 z-0">
