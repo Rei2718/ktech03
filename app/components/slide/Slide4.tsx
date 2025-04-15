327102df39129866f63f1bdcb89b3aac0f24afd8
Ishikawa Ichiro <129850397+Rei2718@users.noreply.github.com>
2025-04-15 18:01:32 +0900

    Slide4.tsx の更新

--- app/components/slide/Slide4.tsx
+++ app/components/slide/Slide4.tsx
@@ -5,7 +5,7 @@ export default function Slide4() {
       {/* 背景画像エリア */}
       <div className="absolute inset-0">
         <img
-          src="Screen04.png"
+          src="gr4.webp"
           alt="背景画像"
           className="w-full h-full object-cover object-center"
         />
