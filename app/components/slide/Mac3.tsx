69760049c14f1b2a396894b78a37d360e0216f7f
Ishikawa Ichiro <129850397+Rei2718@users.noreply.github.com>
2025-04-15 18:15:22 +0900

    Mac3.tsx の更新

--- app/components/slide/Mac3.tsx
+++ app/components/slide/Mac3.tsx
@@ -11,7 +11,7 @@ export default function Mac3() {
       </div>
       <div className="absolute inset-0 z-0">
         <img
-          src="Screen01.png"
+          src="gr9.webp"
           alt="mac1"
           className="w-full h-full object-cover brightness-70"
         />
