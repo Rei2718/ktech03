ceaad3d086486225488dc1b9ccba95c1d59a3000
Ishikawa Ichiro <129850397+Rei2718@users.noreply.github.com>
2025-04-15 18:15:01 +0900

    Mac2.tsx の更新

--- app/components/slide/Mac2.tsx
+++ app/components/slide/Mac2.tsx
@@ -11,7 +11,7 @@ export default function Mac2() {
       </div>
       <div className="absolute inset-0 z-0">
         <img
-          src="Screen01.png"
+          src="gr8.webp"
           alt="mac1"
           className="w-full h-full object-cover brightness-70"
         />
