7dc60dd6b9bc122a8f6ece477e4163c78f4a0385
Ishikawa Ichiro <129850397+Rei2718@users.noreply.github.com>
2025-04-15 18:16:19 +0900

    Mac5.tsx の更新

--- app/components/slide/Mac5.tsx
+++ app/components/slide/Mac5.tsx
@@ -11,7 +11,7 @@ export default function Mac5() {
         </div>
         <div className="absolute inset-0 z-0">
           <img
-            src="Screen01.png"
+            src="gr11.webp"
             alt="mac1"
             className="w-full h-full object-cover brightness-70"
           />
