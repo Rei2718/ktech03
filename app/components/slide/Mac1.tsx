ba039263f20a3196af333b785b2d136580b9c01f
Ishikawa Ichiro <129850397+Rei2718@users.noreply.github.com>
2025-04-15 18:21:36 +0900

    Mac1.tsx の更新

--- app/components/slide/Mac1.tsx
+++ app/components/slide/Mac1.tsx
@@ -6,7 +6,7 @@ export default function Mac1() {
       <div className="absolute top-0 left-0 z-10 flex flex-col justify-between h-full px-6 sm:px-10 md:px-16 lg:px-24 py-10 sm:py-12 md:py-20">
         <div className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-semibold text-left">
           <div className="pb-6">COMPUTER GRAPHICS</div>
-          <div className="text-base sm:text-lg md:text-xl lg:text-xl xl:text-2xl max-w-3xl mx-auto text-[#A2A2A2]">
+          <div className="text-base sm:text-lg md:text-xl lg:text-xl xl:text-2xl max-w-3xl mx-auto">
             Blender環境構築とモデリング基礎
           </div>
         </div>
