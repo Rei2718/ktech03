44126f75e6c2eb24c948c7a837ef7fb62409bacb
Ishikawa Ichiro <129850397+Rei2718@users.noreply.github.com>
2025-04-15 18:30:44 +0900

    Mac1.tsx の更新

--- app/components/slide/Mac1.tsx
+++ app/components/slide/Mac1.tsx
@@ -1,14 +1,12 @@
 /* eslint-disable @next/next/no-img-element */
 export default function Mac1() {
   return (
-    <div className="relative w-full h-screen bg-black/90 overflow-hidden">
-      <div className="absolute inset-0 pointer-events-none" />
+    <div className="relative w-full h-full bg-black/90 overflow-hidden">
+      <div className="absolute top-0 left-0 w-full h-full pointer-events-none" />
       <div className="absolute top-0 left-0 z-10 flex flex-col justify-between h-full px-6 sm:px-10 md:px-16 lg:px-24 py-10 sm:py-12 md:py-20">
         <div className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-semibold text-left">
           <div className="pb-6">COMPUTER GRAPHICS</div>
-          <div className="text-base sm:text-lg md:text-xl lg:text-xl xl:text-2xl max-w-3xl mx-auto">
-            Blender環境構築とモデリング基礎
-          </div>
+          <div className="text-base sm:text-lg md:text-xl lg:text-xl xl:text-2xl max-w-3xl mx-auto">Blender環境構築とモデリング基礎</div>
         </div>
       </div>
       <div className="absolute inset-0 z-0">
