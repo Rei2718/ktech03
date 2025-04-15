7dba2666e7125839ab0eaccfa64806dc462eb05c
Ishikawa Ichiro <129850397+Rei2718@users.noreply.github.com>
2025-04-15 18:21:52 +0900

    Mac2.tsx の更新

--- app/components/slide/Mac2.tsx
+++ app/components/slide/Mac2.tsx
@@ -6,7 +6,7 @@ export default function Mac2() {
       <div className="absolute top-0 left-0 z-10 flex flex-col justify-between h-full px-6 sm:px-10 md:px-16 lg:px-24 py-10 sm:py-12 md:py-20">
         <div className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-semibold text-left">
           <div className="pb-6">WEB ENGINEERING</div>
-          <div className="text-base sm:text-lg md:text-xl lg:text-xl xl:text-2xl max-w-3xl mx-auto text-[#A2A2A2]">HTML・CSS・JavaScriptフロントエンド基礎</div>
+          <div className="text-base sm:text-lg md:text-xl lg:text-xl xl:text-2xl max-w-3xl mx-auto">HTML・CSS・JavaScriptフロントエンド基礎</div>
         </div>
       </div>
       <div className="absolute inset-0 z-0">
