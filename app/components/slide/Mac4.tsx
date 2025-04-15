fb213f5e573ccfc62cce48bba05406a0d00a517f
Ishikawa Ichiro <129850397+Rei2718@users.noreply.github.com>
2025-04-15 18:19:47 +0900

    Mac4.tsx の更新

--- app/components/slide/Mac4.tsx
+++ app/components/slide/Mac4.tsx
@@ -13,7 +13,7 @@ export default function Mac4() {
           <img
             src="gr10.webp"
             alt="mac1"
-            className="w-full h-full object-cover brightness-70"
+            className="w-full h-full object-cover brightness-90"
           />
         </div>
       </div>
